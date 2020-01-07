55decba18e3177cfcd8dd10d1a7b329763e09156
Darin Sorrentino <Darin.Sorrentino@gmail.com>
2020-01-07 17:48:45 -0500

    Troubleshooting.

--- tower/configure_tower.sh
+++ tower/configure_tower.sh
@@ -72,8 +72,8 @@ do
 	sleep 10
 done
 
-RH_INSTALLER_RHN_ROLE=$(sudo ls /var/lib/awx/projects/ | grep rh_installer | grep lock | sed 's/.lock//g')
-RH_INSTALLER_RHN_ROLE=/var/lib/awx/projects/${RH_INSTALLER_RHN_ROLE}/playbooks/rhel_node/roles/rhn
+RH_INSTALL_LOCAL_DIR=/var/lib/awx/project/rh_installer.local
+
 echo ""
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo ""
@@ -84,7 +84,7 @@ echo "for registering a node to RHN. It will support using a differer Pool ID"
 echo "for each product."
 echo ""
 echo "The encrypted files can be found in:"
-echo "  ${RH_INSTALLER_RHN_ROLE}/vars/main.yml"
+echo "  ${RH_INSTALL_LOCAL_DIR}"
 echo ""
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo ""
@@ -107,30 +107,30 @@ done
 read -p "[RHEL] Enter RHN Pool for RHEL: " RHEL_POOL
 read -p "[Openstack] Enter RHN Pool for Openstack: " OPENSTACK_POOL
 
-sudo mkdir -p ${RH_INSTALLER_RHN_ROLE}/vars
+sudo mkdir -p ${RH_INSTALL_LOCAL_DIR}/vars
 
 VAULT_PW_FILE=$(mktemp -p ~)
 chmod 600 ${VAULT_PW_FILE}
-if [[ -z "$(sudo ls ${RH_INSTALLER_RHN_ROLE}/vars/vault_pw.txt 2>/dev/null)" ]]
+if [[ -z "$(sudo ls ${RH_INSTALL_LOCAL_DIR}/vars/vault_rhn_pw.txt 2>/dev/null)" ]]
 then
 	head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20  >${VAULT_PW_FILE}
 else
-	sudo cat ${RH_INSTALLER_RHN_ROLE}/vars/vault_pw.txt >${VAULT_PW_FILE}
+	sudo cat ${RH_INSTALL_LOCAL_DIR}/vars/vault_rhn_pw.txt >${VAULT_PW_FILE}
 fi
 
 LOG "stdout" "Creating Vault credential."
 RESULT=$(CREATE_CREDENTIAL "${TOWER_ORG}" "${CRED_RH_INSTALLER_VAULT_NAME}" "Vault" "Password" "$(cat ${VAULT_PW_FILE})")
 
-LOG "stdout" "Storing encrypted data in ${RH_INSTALLER_RHN_ROLE}/vars/main.yml"
+LOG "stdout" "Storing encrypted data in ${RH_INSTALL_LOCAL_DIR}/vars/rhn.yml"
 echo ""
-echo "rhn_user: darin.sorrentino" | sudo tee ${RH_INSTALLER_RHN_ROLE}/vars/main.yml
-echo -n "${RHN_PASSWORD}" | ansible-vault encrypt_string --vault-password-file ${VAULT_PW_FILE} --stdin-name 'rhn_password' | sudo tee -a ${RH_INSTALLER_RHN_ROLE}/vars/main.yml
-echo -n "${RHEL_POOL}" | ansible-vault encrypt_string --vault-password-file ${VAULT_PW_FILE} --stdin-name 'rhel_pool' | sudo tee -a ${RH_INSTALLER_RHN_ROLE}/vars/main.yml
-echo -n "${OPENSTACK_POOL}" | ansible-vault encrypt_string --vault-password-file ${VAULT_PW_FILE} --stdin-name 'openstack_pool' | sudo tee -a ${RH_INSTALLER_RHN_ROLE}/vars/main.yml
+echo "rhn_user: darin.sorrentino" | sudo tee ${RH_INSTALL_LOCAL_DIR}/vars/rhn.yml
+echo -n "${RHN_PASSWORD}" | ansible-vault encrypt_string --vault-password-file ${VAULT_PW_FILE} --stdin-name 'rhn_password' | sudo tee -a ${RH_INSTALL_LOCAL_DIR}/vars/rhn.yml
+echo -n "${RHEL_POOL}" | ansible-vault encrypt_string --vault-password-file ${VAULT_PW_FILE} --stdin-name 'rhel_pool' | sudo tee -a ${RH_INSTALL_LOCAL_DIR}/vars/rhn.yml
+echo -n "${OPENSTACK_POOL}" | ansible-vault encrypt_string --vault-password-file ${VAULT_PW_FILE} --stdin-name 'openstack_pool' | sudo tee -a ${RH_INSTALL_LOCAL_DIR}/vars/rhn.yml
 
-sudo mv ${VAULT_PW_FILE} ${RH_INSTALLER_RHN_ROLE}/vars/vault_pw.txt
-sudo chmod 0600 ${RH_INSTALLER_RHN_ROLE}/vars/vault_pw.txt
-sudo chown -R awx:awx ${RH_INSTALLER_RHN_ROLE}/vars
+sudo mv ${VAULT_PW_FILE} ${RH_INSTALL_LOCAL_DIR}/vars/vault_rhn_pw.txt
+sudo chmod 0600 ${RH_INSTALL_LOCAL_DIR}/vars/vault_rhn_pw.txt
+sudo chown -R awx:awx ${RH_INSTALL_LOCAL_DIR}/vars
 
 for NDX in $(echo "${!TEMPLATE[@]}" | sed 's/,[a-zA-Z][a-zA-Z]*/ /g' |  xargs -n1 | sort -u)
 do
